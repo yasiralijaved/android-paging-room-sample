b175ce01c3b64562ad0f97dc524a67af5d4aef5b
Yasir Ali <ali.yasir0@gmail.com>
2018-09-07 13:21:04 +0500

    Initial Commit
    Add blank project

---
+++ app/src/main/java/com/yasiralijaved/pagingroomsample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.yasiralijaved.pagingroomsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yasiralijaved.pagingroomsample.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/yasiralijaved/pagingroomsample/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.yasiralijaved.pagingroomsample.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
